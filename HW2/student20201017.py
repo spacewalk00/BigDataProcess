1fe54d7b4e2a98ab8c4be4938398a3f4293890f6
spacewalk00 <ju001217@gmail.com>
2022-10-12 18:41:01 +0900

    submit hw2

---
+++ HW2/student20201017.py
@@ -0,0 +1,64 @@
+import openpyxl
+
+wb = openpyxl.load_workbook( "student.xlsx" )
+ws = wb['Sheet1']
+
+row_id = 1
+student_num = 0
+total = [] 
+for row in ws:
+	if row_id != 1:
+		sum_v = ws.cell(row = row_id, column = 3).value * 0.3
+		sum_v += ws.cell(row = row_id, column = 4).value * 0.35
+		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
+		sum_v += ws.cell(row = row_id, column = 6).value
+		ws.cell(row = row_id, column = 7).value = sum_v
+		total.append(sum_v)
+		student_num += 1	
+	row_id += 1
+
+# grade
+total_sorted = [] 
+total_sorted = sorted(total)
+total_sorted.reverse()
+
+rank = []
+for i in total:
+	rank.append(total_sorted.index(i)+1)
+'''print(rank)'''
+
+grade_list = []
+abc = [ [], [], [] ]
+
+for r in rank:
+	if student_num * 0.3 >= r:
+		grade = "A"
+		abc[0].append(r)
+	elif student_num * 0.7 >= r:
+		grade = "B"
+		abc[1].append(r)
+	else:
+		grade = "C"
+		abc[2].append(r)
+
+	grade_list.append(grade)	
+	
+'''print(abc)
+print(grade_list)'''
+
+for a in range(len(abc)):
+	sorted_abc = sorted(abc[a])
+	for b in range(len(abc[a])):
+		if b < len(abc[a])/2:
+			grade_list[rank.index(sorted_abc[b])] += '+'
+		else:
+			grade_list[rank.index(sorted_abc[b])] += '0'
+	
+'''print(grade_list)'''
+row_id = 1 
+for row in ws:
+	if row_id != 1:	
+		ws.cell(row = row_id, column = 8).value = grade_list.pop(0) 
+	row_id += 1
+
+wb.save("student.xlsx")
