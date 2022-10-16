1445af9241bec609de6d68434c9547acd8ff5947
spacewalk00 <ju001217@gmail.com>
2022-10-16 23:01:32 +0900

    update HW2

--- HW2/student20201017.py
+++ HW2/student20201017.py
@@ -25,36 +25,54 @@ total_sorted.reverse()
 rank = []
 for i in total:
 	rank.append(total_sorted.index(i)+1)
-'''print(rank)'''
+#print(rank)
 
 grade_list = []
 abc = [ [], [], [] ]
 
-for r in rank:
-	if student_num * 0.3 >= r:
-		grade = "A"
-		abc[0].append(r)
-	elif student_num * 0.7 >= r:
-		grade = "B"
-		abc[1].append(r)
-	else:
+overlap = 0
+check = set(rank)
+
+if len(check) != student_num:
+	overlap = 1	
+
+if overlap == 0:
+	for r in rank:
+		if student_num * 0.3 >= r:
+			grade = "A"
+			abc[0].append(r)
+		elif student_num * 0.7 >= r:
+			grade = "B"
+			abc[1].append(r)
+		else:
+			grade = "C"
+			abc[2].append(r)
+
+		grade_list.append(grade)	
+else:
+	for r in rank:
 		grade = "C"
 		abc[2].append(r)
+		grade_list.append(grade)	
+		
+		
+#print(abc)
+#print(grade_list)
 
-	grade_list.append(grade)	
-	
-'''print(abc)
-print(grade_list)'''
 
-for a in range(len(abc)):
-	sorted_abc = sorted(abc[a])
-	for b in range(len(abc[a])):
-		if b < len(abc[a])/2:
-			grade_list[rank.index(sorted_abc[b])] += '+'
-		else:
-			grade_list[rank.index(sorted_abc[b])] += '0'
-	
-'''print(grade_list)'''
+if overlap == 0:		
+	for a in range(len(abc)):
+		sorted_abc = sorted(abc[a])
+		for b in range(len(abc[a])):
+			if b < len(abc[a])/2:
+				grade_list[rank.index(sorted_abc[b])] += '+'
+			else:
+				grade_list[rank.index(sorted_abc[b])] += '0'
+else:
+	for i in range(len(grade_list)):
+		grade_list[i] += '0'
+			
+#print(grade_list)
 row_id = 1 
 for row in ws:
 	if row_id != 1:	
