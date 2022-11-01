4cdaf4b9d7fe62fe44b4e341713e2bfa1ab81bd5
spacewalk00 <ju001217@gmail.com>
2022-11-01 23:18:50 +0900

    submit hw3_uber

--- HW3/UBERStudent20201017.py
+++ HW3/UBERStudent20201017.py
@@ -3,21 +3,33 @@
 import sys
 import datetime
 
-#with open("uber.txt", "rt") as fp:
-with open(sys.argv[1], "rt") as fp:
+file = open("uberoutput.txt", "wt")
+days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+weekday = [{}, {}, {}, {}, {}, {}, {}]
+
+with open("uber.txt", "rt") as fp:
+#with open(sys.argv[1], "rt") as fp:
 	lines = fp.readlines()
 	for line in lines:
 		fields = line.split(",")
+		
+		region = fields[0]
+		date = fields[1].split("/")
+		v = fields[2]
+		t = fields[3]
 
-		date_slash = fields[1]
-		date = date_slash.split("/")	
-
-		days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-		dayObject = datetime.date(int(date[2]), int(date[0]), int(date[1]))
-		wd = days[dayObject.weekday()] 
+		wd_index = datetime.date(int(date[2]), int(date[0]), int(date[1])).weekday()
+		if region in weekday[wd_index].keys():
+			vehicles = int(weekday[wd_index][region].split(',')[0]) + int(v)
+			trips = int(weekday[wd_index][region].split(',')[1]) + int(t)
+			weekday[wd_index][region] = str(vehicles) + "," + str(trips)
+		else:
+			weekday[wd_index][region] = v + "," + t
+for i in range(7):
+	for r in weekday[i].keys(): 		
+		file.write("%s,%s %s\n" %(r, days[i], weekday[i][r]))	
+		
+		
+		
 			
-		#with open("uberoutput.txt", "at") as fw:
-		with open(sys.argv[2], "at") as fw:
-			fw.write(line.replace(date_slash + ",", wd + " "))	
-			
-
+file.close()			
